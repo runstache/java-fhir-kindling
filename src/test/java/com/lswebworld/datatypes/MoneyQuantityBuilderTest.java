ccc7ed6483a24891cff54f9fc06357a751d75182
Larry Shields <larry.shields@gmail.com>
2021-03-06 11:53:09 -0500

    Added Money Quantity Builder with Tests.

---
+++ src/test/java/com/lswebworld/datatypes/MoneyQuantityBuilderTest.java
@@ -0,0 +1,67 @@
+package com.lswebworld.datatypes;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.math.BigDecimal;
+import org.hl7.fhir.r4.model.MoneyQuantity;
+import org.hl7.fhir.r4.model.Quantity.QuantityComparator;
+import org.junit.jupiter.api.Test;
+
+class MoneyQuantityBuilderTest {
+
+  @Test
+  void testWithValue() {
+    MoneyQuantity quantity = new MoneyQuantityBuilder()
+        .withValue(25.5)
+        .build();
+    assertThat(quantity.getValue())
+        .as("Value should not be null")
+        .isNotNull();
+    assertThat(quantity.getValue())
+        .as("Value should be 25.5")
+        .extracting(BigDecimal.class::cast)
+        .extracting(BigDecimal::doubleValue)
+        .isEqualTo(25.5);
+  }
+
+  @Test
+  void testWithComparator() {
+    MoneyQuantity quantity = new MoneyQuantityBuilder()
+        .withComparator(QuantityComparator.GREATER_THAN)
+        .build();
+    assertThat(quantity.getComparator())
+        .as("Comparator should be Greater Than")
+        .isEqualTo(QuantityComparator.GREATER_THAN);
+  }
+
+  @Test
+  void testWithUnit() {
+    MoneyQuantity quantity = new MoneyQuantityBuilder()
+        .withUnit("ml")
+        .build();
+    assertThat(quantity.getUnit())
+        .as("Unit should be ml")
+        .isEqualToIgnoringCase("ml");
+  }
+
+  @Test
+  void testWithSystem() {
+    MoneyQuantity quantity = new MoneyQuantityBuilder()
+        .withSystem("http://unitsofmeasure.org")
+        .build();
+    assertThat(quantity.getSystem())
+        .as("System should be http://unitsofmeasure.org")
+        .isEqualToIgnoringCase("http://unitsofmeasure.org");
+  }
+
+  @Test
+  void testWithCode() {
+    MoneyQuantity quantity = new MoneyQuantityBuilder()
+        .withCode("ml")
+        .build();
+    assertThat(quantity.getCode())
+        .as("Code should be ml")
+        .isEqualToIgnoringCase("ml");
+  }
+  
+}
