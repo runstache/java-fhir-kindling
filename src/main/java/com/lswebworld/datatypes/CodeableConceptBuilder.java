0243dacb595eadf27125a8a010f89741f2a25609
Larry Shields <larry.shields@gmail.com>
2021-03-08 07:24:37 -0500

    Added Codeable Concept Builder with Tests.

---
+++ src/main/java/com/lswebworld/datatypes/CodeableConceptBuilder.java
@@ -0,0 +1,48 @@
+package com.lswebworld.datatypes;
+
+import org.hl7.fhir.r4.model.CodeableConcept;
+import org.hl7.fhir.r4.model.Coding;
+
+public class CodeableConceptBuilder 
+    extends AbstractElementBuilder<CodeableConcept, CodeableConceptBuilder> {
+
+  private CodeableConcept concept;
+
+  /**
+   * Constructor.
+   */
+  public CodeableConceptBuilder() {
+    super();
+    concept = new CodeableConcept();
+    super.initialize(concept);
+  }
+
+  /**
+   * Adds a Coding Object to the Codeable Concept.
+   * @param coding Coding
+   * @return CodeableConceptBuilder
+   */
+  public CodeableConceptBuilder addCoding(Coding coding) {
+    concept.addCoding(coding);
+    return this;
+  }
+
+  /**
+   * Sets the Text on the Codeable Concept.
+   * @param text Text value.
+   * @return CodeableConceptBuilder
+   */
+  public CodeableConceptBuilder withText(String text) {
+    concept.setText(text);
+    return this;
+  }
+
+  /**
+   * Returns the constructed Codeable Concept.
+   * @return Codeable Concept.
+   */
+  public CodeableConcept build() {
+    return concept;
+  }
+  
+}
