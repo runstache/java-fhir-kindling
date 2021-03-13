84066df46ba0949eff1028e1da355adf05bc1ea9
Larry Shields <larry.shields@gmail.com>
2021-03-13 11:55:42 -0500

    Added Contributor Builder with Tests.

---
+++ src/main/java/com/lswebworld/datatypes/ContributorBuilder.java
@@ -0,0 +1,56 @@
+package com.lswebworld.datatypes;
+
+import org.hl7.fhir.r4.model.ContactDetail;
+import org.hl7.fhir.r4.model.Contributor;
+
+public class ContributorBuilder extends AbstractElementBuilder<Contributor, ContributorBuilder> {
+
+  private final Contributor contributor;
+
+  /**
+   * Constructor.
+   */
+  public ContributorBuilder() {
+    super();
+    contributor = new Contributor();
+    super.initialize(contributor);
+  }
+
+  /**
+   * Sets the Contributor Type.
+   * @param type Contributor Type
+   * @return ContributorBuilder
+   */
+  public ContributorBuilder withType(Contributor.ContributorType type) {
+    contributor.setType(type);
+    return this;
+  }
+
+  /**
+   * Sets the Name on the Contributor.
+   * @param name Name
+   * @return ContributorBuilder
+   */
+  public ContributorBuilder withName(String name) {
+    contributor.setName(name);
+    return this;
+  }
+
+  /**
+   * Adds a Contact Detail to the Contributor.
+   * @param detail Contact Detail
+   * @return ContributorBuilder
+   */
+  public ContributorBuilder addContact(ContactDetail detail) {
+    contributor.addContact(detail);
+    return this;
+  }
+
+  /**
+   * Returns the constructed Contributor.
+   * @return Contributor
+   */
+  public Contributor build() {
+    return this.contributor;
+  }
+}
---
+++ src/test/java/com/lswebworld/datatypes/ContributorBuilderTest.java
@@ -0,0 +1,74 @@
+package com.lswebworld.datatypes;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.assertj.core.api.Assertions;
+import org.hl7.fhir.r4.model.ContactDetail;
+import org.hl7.fhir.r4.model.Contributor;
+import org.hl7.fhir.r4.model.Extension;
+import org.hl7.fhir.r4.model.StringType;
+import org.junit.jupiter.api.Test;
+
+class ContributorBuilderTest {
+
+  @Test
+  void testWithId() {
+    Contributor contributor = new ContributorBuilder()
+        .withId("MyId")
+        .build();
+    assertThat(contributor.getId())
+        .as("Id should be My Id")
+        .isEqualToIgnoringCase("MyId");
+  }
+
+  @Test
+  void testAddExtension() {
+    Extension extension = new Extension("http://www.myextension.com");
+    extension.setValue(new StringType("MyValue"));
+
+    Contributor contributor = new ContributorBuilder()
+        .addExtension(extension)
+        .build();
+    Assertions.assertThat(contributor.getExtension())
+        .as("Extension should be set with correct values")
+        .isNotEmpty()
+        .allSatisfy(ext -> {
+          Assertions.assertThat(ext.getUrl()).isEqualToIgnoringCase("http://www.myextension.com");
+          StringType value = (StringType) ext.getValue();
+          Assertions.assertThat(value.getValueAsString()).isEqualToIgnoringCase("MyValue");
+        });
+  }
+
+  @Test
+  void testWithType() {
+    Contributor contributor = new ContributorBuilder()
+        .withType(Contributor.ContributorType.AUTHOR)
+        .build();
+    assertThat(contributor.getType())
+        .as("Contributor type should be Author")
+        .isEqualTo(Contributor.ContributorType.AUTHOR);
+  }
+
+  @Test
+  void testWithName() {
+    Contributor contributor = new ContributorBuilder()
+        .withName("JIM SMITH")
+        .build();
+    assertThat(contributor.getName())
+        .as("Name should be Jim Smith")
+        .isEqualToIgnoringCase("JIM SMITH");
+  }
+
+  @Test
+  void testAddContact() {
+    ContactDetail detail = new ContactDetail();
+    detail.setName("JIM SMITH");
+    Contributor contributor = new ContributorBuilder()
+        .addContact(detail)
+        .build();
+    assertThat(contributor.getContact())
+        .as("Contact Detail should be populated")
+        .isNotEmpty()
+        .allSatisfy(d -> assertThat(d.getName()).isEqualToIgnoringCase("JIM SMITH"));
+  }
+}
